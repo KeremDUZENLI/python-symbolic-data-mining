df90510a7072ae23d34adc4e490deaa21bff3ff4
KeremDUZENLI <krmdznl97@gmail.com>
2025-05-04 22:38:00 +0200

    BACKUP

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-from interface.cli   import CLI
-from interface.gui   import GUI
+from interface.cli import CLI
+from interface.gui import GUI
 
-from algorithms      import ALGORITHMS, run_algorithm
-from helper.dataset  import create_dataset, create_dataset_default, create_dataset_from_grid
-from helper.output   import output_dataset, output_summary
+from algorithms import ALGORITHMS, run_algorithm
+from helper.dataset import create_dataset, create_dataset_default, create_dataset_from_grid
+from helper.output import output_dataset, output_summary
 
 
-# CLI(create_dataset, create_dataset_default, create_dataset_from_grid, ALGORITHMS, run_algorithm, output_dataset, output_summary)
-GUI(create_dataset, create_dataset_default, create_dataset_from_grid, ALGORITHMS, run_algorithm, output_dataset, output_summary)
+def run_cli():
+    CLI(
+        create_dataset_default,
+        create_dataset,
+        create_dataset_from_grid,
+        ALGORITHMS,
+        run_algorithm,
+        output_dataset,
+        output_summary
+    )
+
+
+def run_gui():
+    GUI(
+        create_dataset,
+        create_dataset_default,
+        create_dataset_from_grid,
+        ALGORITHMS,
+        run_algorithm,
+        output_dataset,
+        output_summary
+    )
+
+if __name__ == "__main__":
+    # run_cli()
+    run_gui()
